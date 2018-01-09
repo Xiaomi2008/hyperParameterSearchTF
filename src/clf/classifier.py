8a4290615f625f9819dc99be14bc994b6214865f
Marius Koeppel <m.koeppel@netz98.de>
2018-01-09 13:01:09 +0100

    rename class and add fit funciton from basicClf

--- src/clf/classifier.py
+++ src/clf/classifier.py
@@ -1,9 +1,8 @@
 """ Specific higgsRanker """
 
-import basicClf
 import tensorflow as tf
 
-class clfHandler():
+class classifier():
     """ This class build a clf model and fits this model to train data """
 
     def __init__(self, outputDir, outputName, batch_size, training_size):
@@ -13,10 +12,18 @@ class clfHandler():
         self.outputDir = outputDir
         self.outputName = outputName
         self.outputFile = self.outputDir + self.outputName
+        self._build_model()
+        self.trainingScore = None
 
     def fit(self, data):
-        self._build_model()
-        self._model.fit(data, self.outputFile)
+        """ Train the model """
+        with tf.Session() as sess:
+            saver = tf.train.Saver()
+            sess.run(tf.global_variables_initializer())
+            for _ in range(self.training_size):
+                batch_xs, batch_ys = data.train.next_batch(self.batch_size)
+                sess.run(self.train_step, feed_dict={self.x: batch_xs, self.y_: batch_ys})
+            saver.save(sess, self.outputFile + "_model")
 
     def _build_model(self):
         """ Define the structure of the neural net """
@@ -31,8 +38,15 @@ class clfHandler():
             tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
         train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
-        self._model = basicClf.basicClf(x, W, b, y, y_, cross_entropy, train_step,
-                                        self.batch_size, self.training_size)
+        self.x = x
+        self.W = W
+        self.b = b
+        self.y = y
+        self.y_ = y_
+        self.cross_entropy = cross_entropy
+        self.train_step = train_step
+        self.trainingScore = None
 
     def returnTrainingScores(self):
         """ Return the mean and the std of the fitting from the basicRanker """
+        return self.trainingScore
